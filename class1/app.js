2b6b7c2f094a10b5f8ebaac02b8b2c4a635cbfc4
Umra-Badar20 <umrabadar555@gmail.com>
2024-10-25 17:56:31 +0500

    Initialization

---
+++ class1/app.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyClGjUEM1ZFaaPWtNrm9auRJWZ_iVhc0R8",
+  authDomain: "fir-project-b811d.firebaseapp.com",
+  projectId: "fir-project-b811d",
+  storageBucket: "fir-project-b811d.appspot.com",
+  messagingSenderId: "961312551768",
+  appId: "1:961312551768:web:5de55f3635c27a1eb8eb2d",
+  measurementId: "G-5LTCJ1HPMK"
+};
+
+const app = initializeApp(firebaseConfig);
