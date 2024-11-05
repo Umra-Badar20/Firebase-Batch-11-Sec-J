d12bdbf7e2e3edb3b6f2a7e96a971cf2908961f4
Umra-Badar20 <umrabadar555@gmail.com>
2024-11-05 18:00:21 +0500

    Firestore

--- loginSignup/app.js
+++ loginSignup/app.js
@@ -4,7 +4,15 @@ import {
   signInWithEmailAndPassword,
   onAuthStateChanged,
   sendEmailVerification,
-  signOut,signInWithPopup, GoogleAuthProvider,provider
+  signOut,
+  signInWithPopup,
+  GoogleAuthProvider,
+  provider,
+  getFirestore,
+  db,
+  collection,
+  addDoc,
+  getDocs,doc, setDoc
 } from "./firebase.js";
 
 let signUp = () => {
@@ -14,14 +22,39 @@ let signUp = () => {
   let emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
   let passwordRegex = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
 
+  let name = document.getElementById("name").value;
+  let number = document.getElementById("number").value;
+  let userData = { name, number, email, password };
+  console.log(userData);
+
   if (emailRegex.test(email) && passwordRegex.test(password)) {
     console.log("test");
     createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
+      .then(async (userCredential) => {
         const user = userCredential.user;
-        console.log(user);
+        // console.log(user);
         alert("Account created successfully");
-        window.location.href = "./class27/index.html"
+        // window.location.href = "./class27/index.html"
+        // ________________________________Add Doc
+      //   try {
+      //     const docRef = await addDoc(collection(db, "users"), {
+      //       ...userData,
+      //       uId: user.uid,
+      //     });
+      //     console.log("Document written with ID: ", docRef.id);
+      //   } catch (e) {
+      //     console.error("Error adding document: ", e);
+      //   }
+      // ____________________________________Set Doc
+      try {
+        await setDoc(doc(db,"users",user.uid), {
+          ...userData,
+          uId: user.uid
+        });
+            console.log("Document written with ID: ", user.uid);
+          } catch (e) {
+            console.error("Error adding document: ", e);
+          }
       })
       .catch((error) => {
         console.log(error.message);
@@ -34,53 +67,60 @@ let signUp = () => {
     alert("Passwords should be identical");
   }
 };
-if(window.location.pathname=="/loginSignup/index.html"){
-    let signUp_btn = document.getElementById("signUp_btn");
-    signUp_btn.addEventListener("click", signUp);
+if (window.location.pathname == "/loginSignup/index.html") {
+  let signUp_btn = document.getElementById("signUp_btn");
+  signUp_btn.addEventListener("click", signUp);
 }
 let logIn = () => {
   let email = document.getElementById("email").value;
   let password = document.getElementById("pass").value;
   signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
-     alert("Login Successful")
-     window.location.href = "./class27/index.html"
+      alert("Login Successful");
+      window.location.href = "./class27/index.html";
     })
     .catch((error) => {
-      alert(error.code)
+      alert(error.code);
     });
 };
-if(window.location.pathname=="/loginSignup/login.html"){
-let login_btn = document.getElementById("login_btn");
-login_btn.addEventListener("click", logIn);
+if (window.location.pathname == "/loginSignup/login.html") {
+  let login_btn = document.getElementById("login_btn");
+  login_btn.addEventListener("click", logIn);
 }
-let googleSignup=()=>{
-    signInWithPopup(auth, provider)
+let googleSignup = () => {
+  signInWithPopup(auth, provider)
     .then((result) => {
-     
       const credential = GoogleAuthProvider.credentialFromResult(result);
       const token = credential.accessToken;
       const user = result.user;
-     console.log(user);
-     window.location.href = "./class27/index.html"
-
-    }).catch((error) => {
-      
+      console.log(user);
+      window.location.href = "./class27/index.html";
+    })
+    .catch((error) => {
       const email = error.customData.email;
       const credential = GoogleAuthProvider.credentialFromError(error);
       console.log(email, credential);
     });
+};
+if (window.location.pathname == "/loginSignup/index.html") {
+  let googleBtn = document.getElementById("googleBtn");
+  googleBtn.addEventListener("click", googleSignup);
 }
-if(window.location.pathname=="/loginSignup/index.html"){
-    let googleBtn = document.getElementById("googleBtn");
-    googleBtn.addEventListener("click", googleSignup);
-}
-document.getElementById("logOut").addEventListener("click",()=>{
-    signOut(auth).then(() => {
-        alert("Log out successfull")
-        window.location.href = "../index.html"
-      }).catch((error) => {
-        console.log(error.code);
-        
-      });
-})
+// document.getElementById("logOut").addEventListener("click",()=>{
+//     signOut(auth).then(() => {
+//         alert("Log out successfull")
+//         window.location.href = "../index.html"
+//       }).catch((error) => {
+//         console.log(error.code);
+
+//       });
+// })
+
+//Getting user data from firestore
+let getAllUsers = async () => {
+  const querySnapshot = await getDocs(collection(db, "users"));
+  querySnapshot.forEach((doc) => {
+    console.log(`${doc.id} => `,doc.data());
+  });
+};
+getAllUsers()
